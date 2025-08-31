7f167de569acedc8d6f73d7c9c9fe4dede88ae09
Myzerberries <52772439+Myzerberries@users.noreply.github.com>
2025-08-31 16:23:25 -0500

    Adding API gateways and modified lambda function to include support for API calls to the function.

--- lambda_function.py
+++ lambda_function.py
@@ -7,11 +7,12 @@ import json
 def lambda_handler(event, context):
     execution_id = context.aws_request_id
     print(f"Execution ID: {execution_id}")
-    process_instances(context, execution_id)
 
     q = event.get("queryStringParameters") or {}
     key   = q.get("key")
     value = q.get("value")
+
+    process_instances(context, execution_id, key, value)
 
     return {
         "statusCode": 200,
@@ -39,12 +40,11 @@ def process_instances(context, execution_id, key, value):
         for instance in reservation["Instances"]:
             if instance["State"].get('Name') == 'running':
                     for tag in instance.get("Tags", []):
-                        #print (tag["Key"], tag["Value"])
                         if tag["Key"] == "Name":
                             print (tag["Value"], "is running")
                         if tag["Key"] == "AutoShutdown" and tag["Value"] == "True":
                             autoshutdown = True
-                        if tag["Key"] == "Environment" and tag["Value"] == "Dev" and autoshutdown == True:
+                        if tag["Key"] == "Environment" and tag["Value"] == "Dev" and autoshutdown == True and key == None and value == None:
                             autoshutdown = False
                             stop_instances = ec2.stop_instances(
                             InstanceIds=[
@@ -79,14 +79,44 @@ def process_instances(context, execution_id, key, value):
                                 }
                             )
                             print("PutItem succeeded:", dynamodb_write)
+                        elif key != None and value != None and tag["Key"] == key and tag["Value"] == value and autoshutdown == True:
+                            autoshutdown = False
+                            stop_instances = ec2.stop_instances(
+                            InstanceIds=[
+                                instance.get("InstanceId")
+                            ]
+                        )
+                            waiter = ec2.get_waiter("instance_stopped")
+                            waiter.wait(InstanceIds=[instance["InstanceId"]])
+                            print(instance["InstanceId"], "stopped")
+                            end_time = datetime.utcnow()
+                            start_time = end_time - timedelta(hours=24)
+                            response = cloudtrail.lookup_events(
+                                LookupAttributes=[
+                                    {"AttributeKey": "ResourceName", "AttributeValue": instance["InstanceId"]}
+                                ],
+                                StartTime=start_time,
+                                EndTime=end_time,
+                                MaxResults=5
+                            )
+                            for event in response["Events"]:
+                                if event["EventName"] == "StopInstances":
+                                    shutdown_timestamp = event['EventTime']
+                                    break
 
+                            dynamodb_write = dynamodb.put_item(
+                                TableName='Stopped_Instance_Logging_Table',
+                                Item={
+                                    'InstanceId': {'S': instance["InstanceId"]},
+                                    'TimeStamp': {'S': str(shutdown_timestamp)},
+                                    'Tags': {'S': tag["Key"] + '=' + tag["Value"]},
+                                    'ExecutionId' : {'S' : execution_id}
+                                }
+                            )
+                            print("PutItem succeeded:", dynamodb_write)
             elif instance["State"].get('Name') == 'stopped':
                     for tag in instance.get("Tags", []):
                         if tag["Key"] == "Name":
                             print (tag["Value"], "is stopped")
             else:
                 print ("Instance state unknown")
-
-
-
-# Adding a comment to test PR and merge workflows
