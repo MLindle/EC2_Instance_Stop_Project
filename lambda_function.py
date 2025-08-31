0cc29e6d71984327778e068cbd6c45407bf35b5a
Myzerberries <52772439+Myzerberries@users.noreply.github.com>
2025-08-31 11:56:39 -0500

    Updated script to use 'lambda_handler' and 'process_instances' functions. Did this so I can pass 'ExecutionId' into the DynamoDB table for logging.

--- lambda_function.py
+++ lambda_function.py
@@ -4,81 +4,81 @@ from datetime import datetime, timedelta
 
 import json
 
-# #def lambda_handler(event, context):
-#     execution_id = context.aws_request_id
-#     print(f"Execution ID: {execution_id}")
-#     process_instances(context, execution_id)
-#     return {
-#         "statusCode": 200,
-#         "body": json.dumps({"execution_id": execution_id})
-#     }
+def lambda_handler(event, context):
+    execution_id = context.aws_request_id
+    print(f"Execution ID: {execution_id}")
+    process_instances(context, execution_id)
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"execution_id": execution_id})
+    }
     
 
-#def process_instances(context, execution_id):
+def process_instances(context, execution_id):
 
-ec2 = boto3.client('ec2')
+    ec2 = boto3.client('ec2')
 
-cloudtrail = boto3.client("cloudtrail")
+    cloudtrail = boto3.client("cloudtrail")
 
-dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.client('dynamodb')
 
-describe_instances = ec2.describe_instances()
+    describe_instances = ec2.describe_instances()
 
-autoshutdown = False
+    autoshutdown = False
 
-shutdown_timestamp = ''
+    shutdown_timestamp = ''
 
-for reservation in describe_instances["Reservations"]:
-    for instance in reservation["Instances"]:
-        if instance["State"].get('Name') == 'running':
-                for tag in instance.get("Tags", []):
-                    #print (tag["Key"], tag["Value"])
-                    if tag["Key"] == "Name":
-                        print (tag["Value"], "is running")
-                    if tag["Key"] == "AutoShutdown" and tag["Value"] == "True":
-                        autoshutdown = True
-                    if tag["Key"] == "Environment" and tag["Value"] == "Dev" and autoshutdown == True:
-                        autoshutdown = False
-                        stop_instances = ec2.stop_instances(
-                        InstanceIds=[
-                            instance.get("InstanceId")
-                        ]
-                    )
-                        waiter = ec2.get_waiter("instance_stopped")
-                        waiter.wait(InstanceIds=[instance["InstanceId"]])
-                        print(instance["InstanceId"], "stopped")
-                        end_time = datetime.utcnow()
-                        start_time = end_time - timedelta(hours=24)
-                        response = cloudtrail.lookup_events(
-                            LookupAttributes=[
-                                {"AttributeKey": "ResourceName", "AttributeValue": instance["InstanceId"]}
-                            ],
-                            StartTime=start_time,
-                            EndTime=end_time,
-                            MaxResults=5
+    for reservation in describe_instances["Reservations"]:
+        for instance in reservation["Instances"]:
+            if instance["State"].get('Name') == 'running':
+                    for tag in instance.get("Tags", []):
+                        #print (tag["Key"], tag["Value"])
+                        if tag["Key"] == "Name":
+                            print (tag["Value"], "is running")
+                        if tag["Key"] == "AutoShutdown" and tag["Value"] == "True":
+                            autoshutdown = True
+                        if tag["Key"] == "Environment" and tag["Value"] == "Dev" and autoshutdown == True:
+                            autoshutdown = False
+                            stop_instances = ec2.stop_instances(
+                            InstanceIds=[
+                                instance.get("InstanceId")
+                            ]
                         )
-                        for event in response["Events"]:
-                            if event["EventName"] == "StopInstances":
-                                shutdown_timestamp = event['EventTime']
-                                break
+                            waiter = ec2.get_waiter("instance_stopped")
+                            waiter.wait(InstanceIds=[instance["InstanceId"]])
+                            print(instance["InstanceId"], "stopped")
+                            end_time = datetime.utcnow()
+                            start_time = end_time - timedelta(hours=24)
+                            response = cloudtrail.lookup_events(
+                                LookupAttributes=[
+                                    {"AttributeKey": "ResourceName", "AttributeValue": instance["InstanceId"]}
+                                ],
+                                StartTime=start_time,
+                                EndTime=end_time,
+                                MaxResults=5
+                            )
+                            for event in response["Events"]:
+                                if event["EventName"] == "StopInstances":
+                                    shutdown_timestamp = event['EventTime']
+                                    break
 
-                        dynamodb_write = dynamodb.put_item(
-                            TableName='Stopped_Instance_Logging_Table',
-                            Item={
-                                'PrimaryKey': {'S': instance["InstanceId"]},
-                                'TimeStamp': {'S': str(shutdown_timestamp)},
-                                'Tags': {'S': tag["Key"] + '=' + tag["Value"]},
-                                #'ExecutionId' : {'S' : execution_id}
-                            }
-                        )
-                        print("PutItem succeeded:", dynamodb_write)
+                            dynamodb_write = dynamodb.put_item(
+                                TableName='Stopped_Instance_Logging_Table',
+                                Item={
+                                    'InstanceId': {'S': instance["InstanceId"]},
+                                    'TimeStamp': {'S': str(shutdown_timestamp)},
+                                    'Tags': {'S': tag["Key"] + '=' + tag["Value"]},
+                                    'ExecutionId' : {'S' : execution_id}
+                                }
+                            )
+                            print("PutItem succeeded:", dynamodb_write)
 
-        elif instance["State"].get('Name') == 'stopped':
-                for tag in instance.get("Tags", []):
-                    if tag["Key"] == "Name":
-                        print (tag["Value"], "is stopped")
-        else:
-            print ("Instance state unknown")
+            elif instance["State"].get('Name') == 'stopped':
+                    for tag in instance.get("Tags", []):
+                        if tag["Key"] == "Name":
+                            print (tag["Value"], "is stopped")
+            else:
+                print ("Instance state unknown")
 
 
 
